9afffd860e70de3968467b9501be1a5c2d2e69d6
arnif <arnifa@gmail.com>
2015-03-30 07:42:49 +0000

    port

--- index.js
+++ index.js
@@ -1,22 +1,18 @@
 var mongoose   = require('mongoose'),
  	Entity = require('./models/entity'),
     express = require('express'),
-    cors = require('cors');
+    cors = require('cors'),
+    bodyParser = require('body-parser');
 
 mongoose.connect('mongodb://localhost/wheredidipark'); // connect to our database
 
 var app = express();
-var bodyParser = require('body-parser')
-app.use( bodyParser.json() );       // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-})); 
-
+app.use(bodyParser.json()); // for parsing application/json
 app.use(cors());
 
 require('./lib/routes')(app);
 
-var port = process.env.PORT || 8080;
+var port = process.env.PORT || 8085;
 
 app.listen(port);
 console.log('API live on ', port);
